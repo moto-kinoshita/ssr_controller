6970be51bcbb4190fa0cc7d152ee5d445c3d17c4
moto-kinoshita <42830818+moto-kinoshita@users.noreply.github.com>
2021-01-05 03:47:01 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,131 @@
+import time
+from ssr import SsrDriver
+from temp_reader import TempReader
+from threading import Event
+import sys
+import queue
+import json
+import RPi.GPIO as GPIO
+
+"""
+    メインスクリプト
+    以下のスレッドを起動してSSRを制御する。
+
+    温度取得：TempReader
+    SSR制御：SsrDriver
+    温度データ受け渡しキュー：q_tc_temp
+
+
+"""
+def main():
+ 
+    with open("./config.json", mode="r") as fr:
+        config = json.load(fr)
+
+    print(config)
+
+    rate = 115200   # 通信レート
+
+    # 温度用キュー（現在：全て同じキューを使用）
+    """
+    （以下、変数の中身の例）
+    str.port= /dev/ttyUSB0
+    tc_readers_dictというスレッドの立ち上がり:
+    tc_readers_dict （スレッド）==  {'/dev/ttyUSB0': <TempReader(Thread-1, started 1985582176)>}
+    """
+    q_maxlen = 20
+    tc_readers_dict = {}
+    for str_port in config["Tc"].keys():
+        print("tc_reader USB port=", str_port)
+        tc_index = config["Tc"][str_port]["index"]	# ジェイソン使って順番を指定してもらう。Kメモ{210103}
+        
+        save_file = f"output_{str_port[-1]}.txt"
+        tc_readers_dict[str_port] = TempReader(
+            str_port=str_port, 
+            rate=rate, 
+            tc_index=tc_index, 
+            q_maxlen=q_maxlen, 
+            save_file=save_file
+        )									# とにかく、順番に、温度を読んでくる。Kメモ{210103}
+        tc_readers_dict[str_port].start() # スレッドを起動
+        time.sleep(1)
+    print("tc_readers_dictというスレッドの立ち上がり")   #Kinoshita{210102}
+    print("str.port=", str_port)   #Kinoshita{210102}
+    print("jsonからの情報の持ち込み方を見たい：")   #Kinoshita{210102}
+    print("tc_readers_dict （スレッド）== ",tc_readers_dict)  #Kinoshita{210102}
+
+    # SSR制御スレッド
+    # スレッド起動
+    """
+    SSR のスレッドは以下のように立ち上がる：
+    ssr_group_dict= {
+    0: <SsrDriver(Thread-2, started 1975514208)>, 
+    1: <SsrDriver(Thread-3, started 1965028448)>, 
+    2: <SsrDriver(Thread-4, started 1954542688)>, 
+    3: <SsrDriver(Thread-5, started 1944056928)>}
+    """
+    ssr_group_dict = {}
+    # SSRの各々で現時点では、{210104} ループしている
+    # confis.jsonで指定した、Groupでの ループにする方向ではないかとおもう。
+    for i, target_pin in enumerate(config["SSR"]):   #ここでGroup別にイテレーションLoopに。{201212}
+        ssr_group_dict[i] = SsrDriver(
+            target_pin,
+            tc_readers_dict=tc_readers_dict
+        )
+        ssr_group_dict[i].start()    #ここで起動１
+
+    print("SsrDriverへの情報伝達を見たい：")   #Kinoshita{210103}
+    print("ssr_group_dict （スレッド）==",ssr_group_dict)  #Kinoshita{210103}
+    print()
+    print("ssr_group_dict.keys()=",ssr_group_dict.keys()) #Kinoshita{210103}
+
+    # ここから Ctrl-C が押されるまで無限ループ
+    try:
+        while True:
+            # ここに処理を記載
+            time.sleep(1)
+            # 例
+            """
+            支配層と被支配層とに分けて「ロジック」（やりたいこと）を書き込むのではなくて
+            やりたいことを表現する場を、上手に、被支配層と支配層とを突き抜けて、用意する方法を
+            うまくつくっていく。そういう技法を身に着けなければいけない。
+            複雑なことを、どう整理するか、
+            UML 図　を描いてみようと思います。 Kコメント{210103}
+            """
+            ssr_group_dict[0].set_target_temp(200)   #目標温度をGroup別に。現時点 {210104} では SSRに一対一対応している。
+            ssr_group_dict[1].set_target_temp(200)
+            ssr_group_dict[2].set_target_temp(200)
+            ssr_group_dict[3].set_target_temp(200)
+            # print(f"que_len: {q_tc_temp.qsize()}")
+            pass
+        
+    except KeyboardInterrupt:
+        # Ctrl-C
+        print('interrupted!')
+
+        # SSRスレッド終了処理 ※先に止める
+        for i, ssr in enumerate(ssr_group_dict.keys()): #現時点では、SSRのここに対応している。
+            # print (f"exiting at ssr.join({i})")
+            ssr_group_dict[ssr].close()
+            time.sleep(0.1)
+
+        time.sleep(1)
+
+        # 温度取得スレッド終了処理
+        for i, str_port in enumerate(tc_readers_dict.keys()):
+            # print (f"exiting at temp_reader.join({i})")
+            tc_readers_dict[str_port].close()
+            time.sleep(0.1)
+
+
+        GPIO.cleanup()
+        time.sleep(1)
+
+        print("call exit!")
+        exit()
+
+
+if __name__ == "__main__":
+    
+    # setting.init()
+    main()
---
+++ temp_reader.py
@@ -0,0 +1,137 @@
+import serial, sys, queue
+from collections import deque
+import re
+import time
+from threading import Event, Thread
+import RPi.GPIO as GPIO
+# import setting
+
+"""
+    USBから温度取得用スレッド
+"""
+
+class TempReader(Thread):
+    def __init__(self, str_port, rate, tc_index, q_maxlen, save_file=None):
+        Thread.__init__(self)
+
+        # 温度用のキュー
+        self.tc_index = tc_index
+        self.q_maxlen = q_maxlen
+        self.tc_queue_dict = {}
+        self.tc_now = {}
+        for idx in tc_index:
+            self.tc_queue_dict[idx] = deque(maxlen=q_maxlen)
+            self.tc_now[idx] = None
+
+        # 外部からスレッド停止用
+        self.running = True
+
+        # シリアルポート設定
+        self.str_port = str_port
+        self.rate = rate
+
+        self.ser = serial.Serial(str_port, rate, timeout = 0) #20200627 115200->19200
+        self.ser.send_break()
+        self.ser.reset_input_buffer
+        self.ser.reset_input_buffer
+        self.ser.reset_input_buffer
+        self.ser.reset_input_buffer
+        line_byte = self.ser.readline()  #一回、読み飛ばしをかける
+        # line_byte = line_byte.decode(encoding='utf-8')
+        # temperatures = line_byte.split(',')
+        # print(f"[Debug] line_s = {temperatures}")
+        # self.tc_queue_dict.put(temperatures[2])
+        # print(f"{self.str_port}, {tc_queue_dict.qsize()}")
+        #一回バッファークリアしないといけない？？？
+        self.ser.reset_input_buffer
+        
+        # ログファイル
+        self.fw = open(save_file, "w+")
+
+        time.sleep(0.2)
+
+    def run(self):
+
+        event = Event()
+        event.set()
+
+        time.sleep(3)
+        while self.running:
+            buff_waiting = self.ser.in_waiting
+
+            # print(f"buff_waiting: {buff_waiting}")
+            # バッファーにデータ有
+            if buff_waiting > 0:
+                line_byte = self.ser.readline()
+                line_byte = line_byte.decode(encoding='utf-8')
+                temperatures=line_byte.split(',')
+                self.fw.write(",".join(temperatures))   # ファイルへ保存
+                # print(f"line_s = {temperatures}")
+
+                # キューに温度をいれる（SSR制御で使用）
+                print(f"{self.str_port}: {temperatures}")
+                for idx in self.tc_index:
+                    if True: #debug print
+                      if idx == 1:
+                         print("debug_print{210104}  ",
+                         "time=",temperatures[idx],
+                         "second=",temperatures[1][temperatures[1].rfind(':')+1:]
+                         )
+                    self.tc_queue_dict[idx].append(temperatures[idx])      
+                    self.tc_now = temperatures[idx]     #OBS! {210104} need time_in_sseconds(for dt calculation)
+                # print(f"{self.str_port}, {self.tc_queue_dict.qsize()}")
+                """
+                T_measは、Tc-1なので、これをPIDの計測値としてPID制御をする。
+                tempertures[2]
+                """
+                # q.put(line_byte)
+                # if False :
+                # print(buff_waiting, q.qsize(), line_byte)
+            event.wait()
+        # 終了処理
+        self.ser.close()
+        self.fw.close()
+        print(f"exit usb: {self.str_port}")
+    
+    def close(self):
+        """
+        外部からスレッド停止用メソッド
+        """
+        print(f"close usb: {self.str_port}")
+        self.running = False
+
+    def get_tc_now(self, idx):
+
+        return self.tc_now[idx]
+
+    def get_tc_average(self, idx):
+
+        total = 0
+        for tc_hist in self.tc_queue_dict[idx]:
+            total += tc_hist
+
+        return total / self.q_maxlen
+        
+        
+
+    def comment_210104(self):
+        s1="""
+        K{210104}
+        self.tc_now = temperatures[idx]  で ssrに情報を送り込んでいる。
+        temperatures[idex]の中身は（例えば）以下のようになっている
+                    idx= 0   temperatures[idex]=  02
+                    idx= 1   temperatures[idex]=  188:56:33.2　これが時刻
+                    idx= 2   temperatures[idex]=  11.0625
+                    idx= 3   temperatures[idex]=  10.6875
+                    idx= 4   temperatures[idex]=  10.8750
+                    idx= 5   temperatures[idex]=  11.0625
+                    idx= 6   temperatures[idex]=  11.3125
+                    idx= 7   temperatures[idex]=  11.3125
+                    idx= 8   temperatures[idex]=  11.3125
+                    idx= 9   temperatures[idex]=  11.1250
+        """
+        print(s1)
+
+
+
+        
